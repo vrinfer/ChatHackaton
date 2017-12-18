eca5a3b6f012a7ef4b341f3d4825c7ce4acae819
Facundo Sa <facusa7@hotmail.com>
2017-12-18 16:18:23 -0300

    Creo los controles y agrego vista

---
+++ IMChatApp/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using CustomerChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMChatApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Chat(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Title = "Home Page";
+                ViewBag.Username = model.UserNick;
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
